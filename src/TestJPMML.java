7245d40209b84a3795af5ab10c4c424dc8c8d9e3
Saurabh Agarwal <saurabh.agarwal@lochbridge.com>
2017-01-24 14:47:56 +0530

    Test Commit

--- src/TestJPMML.java
+++ src/TestJPMML.java
@@ -4,5 +4,7 @@ import com.oracle.iot.server.analytics.model.AnalyticsResourcePool;
 import com.oracle.iot.server.analytics.exception.AnalyticsValidationException;
 import com.oracle.iot.server.authorization.AuthorizationServiceImpl;
 public class TestJPMML {
-
+	public static void main(String aa[]){
+		System.out.println("hello world");
+	}
 }
